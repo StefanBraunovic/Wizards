5760574ad8e1c84c2c9e6d3a409f3e6bebac3624
Stefan Braunovic <stefan.braunovic@vegait.rs>
2023-02-20 18:28:49 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState ,useEffect} from 'react';
+
+const { Altura } = require("@altura/altura-js")
+
 
 function App() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetch('/data/textfile.txt')
+      .then(response => response.text())
+      .then(data => {
+        // Process the file contents here
+        setData(data);
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.split('\n').map((row, index) => (
+        <img width={300} height={300} src={`https://ordinals.com/content/${row}`} />
+      ))}
     </div>
   );
 }
+
 
 export default App;
